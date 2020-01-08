c19b228c5e56be01ee05f79ee3eaa933f1b68f8e
Jason Chen <j927chen@edu.uwaterloo.ca>
2020-01-07 19:34:31 -0500

    Initial commit

---
+++ UWShare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UWShare
+//
+//  Created by Jason Chen on 2020-01-07.
+//  Copyright © 2020 Jason Chen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("UW Share")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
